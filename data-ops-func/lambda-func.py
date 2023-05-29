48978e609cb992a283fef945478c7b7f55861431
Michael Nau <michaelwnau@gmail.com>
2023-05-28 23:00:36 -0500

    test lambda-func boilerplate

--- data-ops-func/lambda-func.py
+++ data-ops-func/lambda-func.py
@@ -3,7 +3,14 @@ from random import choices
 
 def lambda_handler(event, context):
     # TODO implement
+    fruit = event['fruit']
+    if fruit == 'cherry':
+        
+        return {
+            'stausCode': 200,
+            'body': json.dumps('I love {fruit}!')
+        }
     return {
-        'stausCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'statusCode': 200,
+        'body': json.dumps('No thanks!')
     }
